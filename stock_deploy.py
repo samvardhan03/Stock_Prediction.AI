a5541ebc85adc776bdf77c62ddfd97e4e9c248d6
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2024-03-29 23:25:26 +0530

    Update stock_deploy.py

--- stock_deploy.py
+++ stock_deploy.py
@@ -58,7 +58,7 @@ if submit_button:
         # Prepare sequences for LSTM
         X_seq = []
         sequence_length = 30
-        for i in range(len(data_scaled) - sequence_length):
+        for i in range(len(data_scaled) - sequence_length + 1):  # Adjusted range
             X_seq.append(data_scaled[i:i+sequence_length, 0])
         X_seq = np.array(X_seq)
         X_seq = X_seq.reshape(X_seq.shape[0], X_seq.shape[1], 1)
